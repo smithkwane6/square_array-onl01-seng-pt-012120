070ca1a7033e6ba28a0d8580b2c5c9be593455ac
Kwane Smith <smithkwane6@gmail.com>
2020-01-28 18:49:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |x|
+      new_array.push x*x
+end
+
+new_array
+
+end
+
+def new_square_array(array)
+  array.collect{|x| x*x}
 end
